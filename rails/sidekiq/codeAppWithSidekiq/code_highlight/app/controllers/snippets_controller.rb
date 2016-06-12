db71c602c508844cd8df1218dd483e2a6f408c3b
rodrigosiqueira <rodrigosiqueiramelo@gmail.com>
2016-06-12 11:35:46 -0300

    Added highlight feature.

--- rails/sidekiq/codeAppWithSidekiq/code_highlight/app/controllers/snippets_controller.rb
+++ rails/sidekiq/codeAppWithSidekiq/code_highlight/app/controllers/snippets_controller.rb
@@ -15,6 +15,9 @@ class SnippetsController < ApplicationController
   def create
     @snippet = Snippet.new(snippet_params)
     if @snippet.save
+      uri = URI.parse('http://pygments.simplabs.com/')
+      request = Net::HTTP.post_form(uri, lang: @snippet.language, code: @snippet.plain_code)
+      @snippet.update_attribute(:highlighted_code, request.body)
       redirect_to @snippet
     else
       render 'new'
