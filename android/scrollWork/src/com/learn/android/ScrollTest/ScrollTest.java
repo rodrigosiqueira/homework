dcff8e08aabf884595e455abe76acc1c74f02cc4
rodrigosiqueira <rodrigosiqueiramelo@gmail.com>
2016-12-12 15:37:36 -0200

    Android layout study

---
+++ android/scrollWork/src/com/learn/android/ScrollTest/ScrollTest.java
@@ -0,0 +1,15 @@
+package com.learn.android.ScrollTest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ScrollTest extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
---
+++ android/tableLayout/src/com/learn/android/TableLayoutTest/TableLayoutTest.java
@@ -0,0 +1,15 @@
+package com.learn.android.TableLayoutTest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TableLayoutTest extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
